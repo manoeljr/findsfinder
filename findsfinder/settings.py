8ddebeae32e77986657795bb428cfd45a6459ffc
Manoel Vieira <manoelv8@gmail.com>
2021-07-09 17:43:01 -0300

    Update settings.py

--- findsfinder/settings.py
+++ findsfinder/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d8)swr+x(y97yodons&*_sks%hv@%((&-9l1=*d@tlzq-m^t+p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
